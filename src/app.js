1c1a86c0f21887b10d6999daeed4a05d042f88c6
H1331 <127679061+H1331@users.noreply.github.com>
2023-05-09 05:24:30 +1300

    html CSS JS added

--- src/app.js
+++ src/app.js
@@ -90,3 +90,32 @@ function displayFahrenheit(event) {
 
 
 
+function displayForecast() {
+    let forecastElement = document.querySelector("#weather-forecast");
+   let days = ["Thu", "Fri", "Sat", "Sun"];
+    let forecastHtml = `<div class="row">`;
+    days.forEach(function (day) {
+        forecastHtml=forecastHtml+
+        `
+                 <div class="col-2">
+                    <div class="weather-forecast-date">
+                        ${day}
+                    </div>
+                    
+                    <img src=" https://openweathermap.org/img/wn/10d@2x.png" alt="" width="36">
+                <div class="weather-forecast-temperature">
+                    <span class="weather-forecast-temperature-max">18°</span>
+                    <span class="weather-forecast-temperature-min">12°</span>
+                </div>
+                </div> `
+        
+    })
+    forecastHtml = forecastHtml + `</div>`;
+    forecastElement.innerHTML = forecastHtml;
+    
+
+}
+
+displayForecast();
+
+
